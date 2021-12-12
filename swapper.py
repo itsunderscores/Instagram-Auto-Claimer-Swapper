3239dc0db746217401a2202502cf8c10533d6381
Underscores <84211119+itsunderscores@users.noreply.github.com>
2021-12-11 18:51:10 -0600

    Update swapper.py

--- swapper.py
+++ swapper.py
@@ -1,16 +1,8 @@
-#from selenium import webdriver
-#from selenium.webdriver.firefox.options import Options
-#from selenium.webdriver.common.proxy import Proxy
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.support import expected_conditions as EC
-#from selenium.webdriver.support.ui import WebDriverWait
-#from seleniumwire import webdriver
 import requests
 import os, sys, time
 import os.path
 import time as t
 import threading
-#import getpass
 import urllib.request
 import urllib.parse
 import pickle
